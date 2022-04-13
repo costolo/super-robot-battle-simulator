bc43117a3c08c15019e076f016c74cd0c9f7ea10
costolo <ben.costolo@gmail.com>
2022-04-13 13:17:13 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import robots from './utils/robots'
 
 function App() {
+  console.log(robots)
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      hello world
     </div>
   );
 }
