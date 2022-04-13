d32ffea9560f62b06cd5a1c93598196720d01994
costolo <ben.costolo@gmail.com>
2022-04-13 13:29:14 -0400

    Add HamburgerIcon.js and begin header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import './App.css'
 import robots from './utils/robots'
+import HamburgerIcon from './icons/HamburgerIcon'
 
 function App() {
-  console.log(robots)
   return (
-    <div className='App'>
-      hello world
+    <div className="App">
+      <div className="App__header-container">
+        <div className="App__hamburger">
+          <HamburgerIcon />
+        </div>
+        <div className="App__header">Super Robot Battle Simulator</div>
+      </div>
+      {/* hamburger */}
+      {/* header */}
+      {/* card container */}
+      {/* fight/reset button */}
+      {/* scrollable robot container */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
